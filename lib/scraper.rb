a1dfa170d4b6b83b17e8ba8946746134cd9f1d2f
Christopher Arias <chrstphr.aaron@gmail.com>
2020-09-06 17:03:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,10 @@
-require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-     student = []
+
   end
 
   def self.scrape_profile_page(profile_url)

a5dbdfc874018a88a0c4884b7a70887d83cf8407
Christopher Arias <chrstphr.aaron@gmail.com>
2020-09-06 17:05:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card a")
+    student_cards.collect do |element|
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
